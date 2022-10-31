583b9e2c2e2120dc1c5015c1e1570d17b4e0d742
3rr0r51 <JK9999345@gmail.com>
2022-10-30 22:17:02 -0700

    explore pt1 fully completed

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,45 @@
 // explore.js
+var voices;
 
 window.addEventListener('DOMContentLoaded', init);
+window.speechSynthesis.addEventListener('voiceschanged',synthInit);
 
-function init() {
-  // TODO
+function init(){
+  document.querySelector('button').addEventListener('click', speak);
+  window.speechSynthesis.addEventListener('change',updateMouth);
 }
+
+function synthInit() {
+  voices = speechSynthesis.getVoices();
+
+  var selector = document.getElementById("voice-select");
+
+  for(var i = 0; i < voices.length; i++){
+    var elem = document.createElement('option');
+    elem.value = i;
+    elem.innerHTML = voices[i]["name"];
+    selector.appendChild(elem);
+  }
+}
+
+
+function speak(){
+  var text = document.getElementById('text-to-speak').value;
+  var voice = document.getElementById("voice-select").value;
+
+  if(isNaN(voice)){
+    return;
+  }
+
+  var textToSay = new SpeechSynthesisUtterance(text);
+  textToSay.voice = voices[voice];
+  speechSynthesis.speak(textToSay);
+
+  textToSay.addEventListener('end', function(){document.querySelector('img').src = "assets/images/smiling.png";});
+  textToSay.addEventListener('start', function(){document.querySelector('img').src = "assets/images/smiling-open.png";});
+}
+
+function updateMouth(){
+  console.log("test");
+}
+
